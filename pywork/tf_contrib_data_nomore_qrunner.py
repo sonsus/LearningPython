ca5aa92c8fd61b048944eb4a7cb4de4870d68778
SEONIL <seonilson92@gmail.com>
2017-08-07 19:52:36 +0900

    no queuerunner anymore

---
+++ pywork/tf_contrib_data_nomore_qrunner.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+import os
+import urllib
+import csv 
+
+'''
+class tf.contrib.data.TextLineDataset
+
+methods:
+.batch (batch_size) : com
+
+'''
+
+
+data = tf.contrib.data.TextLineDataset("./tests/test2.csv").skip(1).batch(10)
+
+itr = data.make_one_shot_iterator()
+batch = itr.get_next()
+
+cols = tf.decode_csv(batch, record_defaults=[[0]]*5)
+
+batch = tf.stack(cols, axis=1)
+
+with tf.Session() as sess:
+    for i in range(10):
+        print(sess.run(batch))
