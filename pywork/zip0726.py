847e00310a7ca5d44fcc58e9a5b60662b2852c22
SEONIL <seonilson92@gmail.com>
2017-11-21 11:41:10 +0900

    built-in zip() function works with np as well

--- pywork/zip0726.py
+++ pywork/zip0726.py
@@ -1,9 +1,15 @@
-lista=[i for i in range(1,5)]
+import numpy as np 
+
+
+lista=list(np.arange(1,4,0.5))
 listb="abcde"
 
 zipped=list(zip(lista,listb))
 print(zipped)
 
+print(zipped[0])
+k=zipped[0]
+print(zipped)
 
 x,y=zip(*zipped)
 print("x is same as lista, but in zip obj" ,list(x)==lista)
@@ -11,7 +17,13 @@ print("y is same as listb, but in zip obj", list(y)==listb)
 
 print("what?")
 
-print(lista)
-print(listb)
-print(list(x))
-print(list(y))
+#print(lista)
+#print(listb)
+print("could zip obj be interpreted as a nparray?")
+print(type(np.array(x)))
+print(np.array(x))
+print(np.array(x)[0])
+print(np.array(x))
+
+#print(list(x))
+#print(list(y))
